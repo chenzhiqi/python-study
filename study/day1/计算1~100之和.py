778aa81615a159677591b7f58a443348a6b13c80
chenzhiqi <55378694+chenzhiqi@users.noreply.github.com>
2019-09-23 14:03:56 +0800

    Create 计算1~100之和.py

---
+++ study/day1/计算1~100之和.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: zhiqi.chen
+# @Date: 2019-04-11 15:40:29
+# @Software: Visual Studio Code
+
+sum = 0 
+for i in range(101):
+    sum = sum + i
+print(sum)
